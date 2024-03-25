8879e044c1260bc2d7945e768c5af06e1ff8e712
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2024-03-26 00:17:44 +0100

    feat: add outline of history controller endpoints

--- backend/src/main/java/edu/ntnu/idatt2105/backend/controller/priv/history/HistoryController.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/controller/priv/history/HistoryController.java
@@ -1,8 +1,13 @@
 package edu.ntnu.idatt2105.backend.controller.priv.history;
 
+import edu.ntnu.idatt2105.backend.dto.quiz.QuizLoadDTO;
+import edu.ntnu.idatt2105.backend.model.quiz.QuizHistory;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping(value = "/api/v1/private/history")
-public class HistoryController {
+public class HistoryController implements IHistoryController{
+    @Override
+    public ResponseEntity<QuizLoadDTO> addHistoricalEvent(@NonNull QuizHistory historicalEvent, @NonNull Authentication authentication) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<QuizLoadDTO> getAllHistoricalEvent(@NonNull Long userId, @NonNull Authentication authentication) {
+        // Could add a check of user preferences to see if historical events should be viewable.
+        return null;
+    }
 }
--- backend/src/main/java/edu/ntnu/idatt2105/backend/controller/priv/history/IHistoryController.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/controller/priv/history/IHistoryController.java
@@ -1,5 +1,22 @@
 package edu.ntnu.idatt2105.backend.controller.priv.history;
 
+import edu.ntnu.idatt2105.backend.dto.quiz.QuizLoadDTO;
+import edu.ntnu.idatt2105.backend.model.quiz.QuizAuthor;
+import edu.ntnu.idatt2105.backend.model.quiz.QuizHistory;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.NonNull;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 /**
  * This interface outlines the various functionality the private endpoint for history's should have.
  *
@@ -8,6 +25,50 @@ package edu.ntnu.idatt2105.backend.controller.priv.history;
  */
 public interface IHistoryController {
 
-    // Add new historical event, get all historical events, get single historical event based on quiz and author_id
+    /**
+     * This method adds a new historical event for a quiz and user.
+     *
+     * @return ResponseEntity showing whether the operation was successful.
+     */
+    @PostMapping(
+            value="/create/event",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "This method adds a new historical event for a user and a given quiz")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful addition of event.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = QuizAuthor.class)) }),
+            @ApiResponse(responseCode = "403", description = "Unauthorized addition of event.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = QuizAuthor.class)) })
+    }
+    )
+    ResponseEntity<QuizLoadDTO> addHistoricalEvent(@RequestBody @NonNull QuizHistory historicalEvent,
+                                                   @NonNull Authentication authentication);
+
+    /**
+     * This method retrieves all historical events for a given user.
+     *
+     * @return ResponseEntity showing whether the operation was successful.
+     */
+    @GetMapping(
+            value="/{userId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "This method retrieves all historical events for a given user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of events.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = QuizAuthor.class)) }),
+            @ApiResponse(responseCode = "403", description = "Unauthorized retrieval of events.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = QuizAuthor.class)) })
+    }
+    )
+    ResponseEntity<QuizLoadDTO> getAllHistoricalEvent(@PathVariable @NonNull Long userId,
+                                                   @NonNull Authentication authentication);
 
 }
