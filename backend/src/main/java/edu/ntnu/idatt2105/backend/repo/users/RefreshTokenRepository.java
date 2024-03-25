39bd456c8966dba12d510ca62915cbc85db1c41d
BrageNTNU <bragehk@stud.ntnu.no>
2024-03-25 16:48:38 +0100

    feat: added docker compose for the entire stack

--- backend/src/main/java/edu/ntnu/idatt2105/backend/repo/users/RefreshTokenRepository.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/repo/users/RefreshTokenRepository.java
@@ -1,6 +1,6 @@
 package edu.ntnu.idatt2105.backend.repo.users;
 
-import edu.ntnu.idatt2105.backend.model.users.Token;
+import edu.ntnu.idatt2105.backend.model.users.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -12,5 +12,5 @@ import org.springframework.stereotype.Repository;
  * @version 1.0 23.03.2024
  */
 @Repository
-public interface TokenRepository extends JpaRepository<Token, Long>, JpaSpecificationExecutor<Token> {
+public interface TokenRepository extends JpaRepository<RefreshToken, Long>, JpaSpecificationExecutor<RefreshToken> {
 }
