72004a2e6d6f6e5005bff30cc04ed6cd4f0707f2
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2024-04-01 13:30:54 +0200

    fix: remove INSTANCE from mapper

--- backend/src/main/java/edu/ntnu/idatt2105/backend/mapper/quiz/MultipleChoiceMapper.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/mapper/quiz/MultipleChoiceMapper.java
@@ -3,12 +3,10 @@ package edu.ntnu.idatt2105.backend.mapper.quiz;
 import edu.ntnu.idatt2105.backend.dto.quiz.question.MultipleChoiceDTO;
 import edu.ntnu.idatt2105.backend.model.quiz.question.MultipleChoice;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper()
+@Mapper(componentModel = "spring")
 public interface MultipleChoiceMapper {
 
-    MultipleChoiceMapper INSTANCE = Mappers.getMapper(MultipleChoiceMapper.class);
 
     default MultipleChoice multipleChoiceDTOToMultipleChoice(MultipleChoiceDTO multipleChoiceDTO) {
         return MultipleChoice
